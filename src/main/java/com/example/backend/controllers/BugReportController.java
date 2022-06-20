a1715bedf0dac4e233acd4a8e33a812461ac05cb
Weeke684 <wouter.schmeetz@home.nl>
2022-06-20 16:05:01 +0200

    Wouter Tried to fix the bug report mail controller

--- src/main/java/com/example/backend/controllers/BugReportController.java
+++ src/main/java/com/example/backend/controllers/BugReportController.java
@@ -1,5 +1,8 @@
 package com.example.backend.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,9 +14,20 @@ import com.example.backend.dto.BugReportDTO;
 @RestController
 public class BugReportController
 {
+	@Autowired
+	private JavaMailSender emailSender;
+	
 	@PostMapping("/bugreport")
 	public boolean SendBugReport(@RequestBody BugReportDTO bugReport)
 	{
+		SimpleMailMessage message = new SimpleMailMessage();
+		message.setTo("overlordjorrit@wt.nl");
+		message.setFrom(bugReport.getContactEmail());
+		message.setSubject(bugReport.getBugReportTitle());
+		message.setText(bugReport.getBugReportMessage());
+		
+		emailSender.send(message);
+		
 		System.out.println("Bug report title: " + bugReport.getBugReportTitle());
 		System.out.println("Bug report message: " + bugReport.getBugReportMessage());
 		System.out.println("Bug report email: " + bugReport.getContactEmail());
