f3bbde8113659a6beb08cb143adfbdbb82c29225
Weeke684 <wouter.schmeetz@home.nl>
2022-06-22 15:43:05 +0200

    W

--- src/main/java/com/example/backend/tables/LoanStatusLog.java
+++ src/main/java/com/example/backend/tables/LoanStatusLog.java
@@ -7,10 +7,12 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
 import org.springframework.format.annotation.DateTimeFormat;
 
 @Entity
+@Table(name = "LoanedBooks")
 public class LoanStatusLog
 {
 	@Id
